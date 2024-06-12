71754c8f04f8020ce8413c2ff6528169c451e51a
PenguinBKDN <son72ltv2@gmail.com>
2024-06-12 08:56:23 +0700

    database connect

--- index.ts
+++ index.ts
@@ -1,1 +1,24 @@
-console.log("OK");
+import express, { Express, Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import * as database from "./config/database";
+
+dotenv.config();
+
+database.connect();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.get("/topics", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ topics: ["Music", "Movies", "Sports"] });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
